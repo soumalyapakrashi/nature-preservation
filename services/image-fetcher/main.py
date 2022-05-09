55f6f9b0049198e380a5998138d4b5c6394ca56e
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2022-05-09 18:00:47 +0530

    Update database after generating image files

--- services/image-fetcher/main.py
+++ services/image-fetcher/main.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import shutil
 import subprocess
+from dotenv import load_dotenv
+import mariadb
+
+load_dotenv()
 
 parser = argparse.ArgumentParser()
 
@@ -33,6 +37,22 @@ if(__name__ == "__main__"):
 
     args = parser.parse_args()
 
+    # Setup connection to database
+    try:
+        connection = mariadb.connect(
+            user = os.environ.get("MYSQL_USER"),
+            password = os.environ.get("MYSQL_PASSWORD"),
+            host = os.environ.get("MYSQL_HOST"),
+            port = int(os.environ.get("MYSQL_PORT")),
+            database = os.environ.get("MYSQL_DATABASE")
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        exit(1)
+    
+    # Get the cursor to the database
+    cursor = connection.cursor()
+
     # Generate the output path
     output_path = os.path.join("D:/Programs/nature-preservation/storage/satellite_data", args.area, args.year)
     os.makedirs(output_path, exist_ok = True)
@@ -46,6 +66,7 @@ if(__name__ == "__main__"):
 
             # Copy only bands 2, 3, 4, and 8
             if(image.endswith("B02.jp2") or image.endswith("B03.jp2") or image.endswith("B04.jp2") or image.endswith("B08.jp2")):
+                # Generate the command to be executed
                 command = ["gdalwarp", "-te"]
                 for coord in args.crop_coords:
                     command.append(coord)
@@ -56,15 +77,38 @@ if(__name__ == "__main__"):
                 command.append(os.path.join(args.image_path, image))
                 command.append(os.path.join(output_path, image))
 
-                # print(command)
-
+                # Execute the command
                 subprocess.run(
                     command,
                     capture_output = True,
-                    text = True
+                    text = True,
+                    check = True
                 )
         
         else:
             # Copy only bands 2, 3, 4, and 8
             if(image.endswith("B02.jp2") or image.endswith("B03.jp2") or image.endswith("B04.jp2") or image.endswith("B08.jp2")):
                 shutil.copy2(os.path.join(args.image_path, image), output_path)
+        
+    
+    # Add the area to database if not already present
+    cursor.execute("SELECT area_id FROM AREAS WHERE area_name = ?", (args.area,))
+
+    if(cursor.rowcount == 0):
+        cursor.execute("INSERT INTO AREAS (area_name) VALUES (?)", (args.area,))
+        area_id = cursor.lastrowid
+    else:
+        for (id,) in cursor:
+            area_id = id
+    
+    # Add the new image path data to database
+    cursor.execute(
+        "INSERT INTO STORED_DATA_INFO (area_id, year, sat_data_path) VALUES (?, ?, ?)",
+        (area_id, args.year, output_path)
+    )
+
+    # Commit the changes made to the database
+    connection.commit()
+
+    # Close the connection to the database
+    connection.close()
