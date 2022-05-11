8f9699597f561f7bf71febeb945c3b49a86daf99
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2022-05-11 17:22:27 +0530

    Delete unused code

--- services/ndvi-generator/visualizations.py
+++ services/ndvi-generator/visualizations.py
@@ -96,31 +96,3 @@ def visualizeNDVICategorical(ndvi_image: np.ndarray) -> np.ndarray:
     final_ndvi_image = cv2.merge([band_blue_image, band_green_image, band_red_image])
             
     return final_ndvi_image
-
-
-# Function to visualize the change map obtained by differencing NDVI of two years.
-# Input is a numpy array containing the change map (values ranging between -2 and 2).
-# Output will be a JPG file.
-def visualizeChangeMap(change_map: np.ndarray) -> None:
-    pass
-
-
-
-if(__name__ == "__main__"):
-    # band_red_image = plt.imread("C:/Users/User/Downloads/L1C_T43REL_A008396_20170130T053159/S2A_MSIL1C_20170130T053041_N0204_R105_T43REL_20170130T053159.SAFE/GRANULE/L1C_T43REL_A008396_20170130T053159/IMG_DATA/T43REL_20170130T053041_B04.jp2")
-
-    # band_green_image = plt.imread("C:/Users/User/Downloads/L1C_T43REL_A008396_20170130T053159/S2A_MSIL1C_20170130T053041_N0204_R105_T43REL_20170130T053159.SAFE/GRANULE/L1C_T43REL_A008396_20170130T053159/IMG_DATA/T43REL_20170130T053041_B03.jp2")
-
-    # band_blue_image = plt.imread("C:/Users/User/Downloads/L1C_T43REL_A008396_20170130T053159/S2A_MSIL1C_20170130T053041_N0204_R105_T43REL_20170130T053159.SAFE/GRANULE/L1C_T43REL_A008396_20170130T053159/IMG_DATA/T43REL_20170130T053041_B02.jp2")
-
-    # plt.imsave("./band.jpg", band_red_image)
-
-    # visualizeRGB(band_red_image, band_green_image, band_blue_image)
-
-    red = cv2.imread("C:/Users/User/Downloads/L1C_T43REL_A015146_20200130T053806/S2B_MSIL1C_20200130T053049_N0208_R105_T43REL_20200130T090639.SAFE/GRANULE/L1C_T43REL_A015146_20200130T053806/IMG_DATA/T43REL_20200130T053049_B04.jp2", cv2.IMREAD_GRAYSCALE)
-
-    green = cv2.imread("C:/Users/User/Downloads/L1C_T43REL_A015146_20200130T053806/S2B_MSIL1C_20200130T053049_N0208_R105_T43REL_20200130T090639.SAFE/GRANULE/L1C_T43REL_A015146_20200130T053806/IMG_DATA/T43REL_20200130T053049_B03.jp2", cv2.IMREAD_GRAYSCALE)
-
-    blue = cv2.imread("C:/Users/User/Downloads/L1C_T43REL_A015146_20200130T053806/S2B_MSIL1C_20200130T053049_N0208_R105_T43REL_20200130T090639.SAFE/GRANULE/L1C_T43REL_A015146_20200130T053806/IMG_DATA/T43REL_20200130T053049_B02.jp2", cv2.IMREAD_GRAYSCALE)
-
-    visualizeRGB(red, green, blue)
